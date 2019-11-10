7839fb5d5c6604ed02a7777cb9ca6ff71b909537
brendaokumu <32571251+brendaokumu@users.noreply.github.com>
2019-11-10 20:33:29 +0300

    update phone intent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -45,7 +45,7 @@ public class InfoActivity extends AppCompatActivity {
 
     public void createPhoneIntent(View view){
         Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
-        phoneIntent.setData(Uri.parse("tel:0713232717"));
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
         startActivity(phoneIntent);
     }
 }
